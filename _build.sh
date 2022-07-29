7fff4ab6d1494625b68d333d4a92fb8c920cd6f7
Gustavo Ramos Rehermann <rehermann6046@gmail.com>
2022-07-29 18:41:25 -0300

    GitLab CI: debug print ldd of ./ucc-bin

--- _build.sh
+++ _build.sh
@@ -80,6 +80,7 @@ cleanup() {
             #WINEPREFIX="$wineprefix" wine "$umake" "$package-$build"
             if [[ -f "$packagefull.u" ]]; then rm "$packagefull.u"; fi
             echo "* Invoking ucc make in $(pwd)"
+            ldd -v ./ucc-bin || true
             "$ucc" make -NoBind ini="$TMP_INI" | tee "$packagedir/make.log"
 
             # Ensure .u is built
